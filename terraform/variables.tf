6a4b5cbd429e61ff9ffafeb0e04274563950ee18
Patrick <patty6339@gmail.com>
2025-07-17 13:19:01 +0300

    cleanup file corrected

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,11 +2,11 @@ variable "region" {
     default = "us-east-1"
 }
 variable "public_key" {
-
+  default = "dummy-key"
 }
 variable "private_key" {
-
+  default = "dummy-key"
 }
 variable "key_name" {
-
+  default = "dummy-key-name"
 }
